59d715653d7908daa87f0c726ac72d69130665c0
Yonatan Malka <y.yonatanmalka@gmail.com>
2025-07-02 17:42:12 +0300

    Force RTL for fresh build

--- Manager_App/components/LoginScreen.jsx
+++ Manager_App/components/LoginScreen.jsx
@@ -87,7 +87,7 @@ const LoginScreen = () => {
             <Text>Don't have account? Sign Up!</Text>
             <Button title="Sign Up" onPress={()=> navigation.navigate("SignUp")}/>
             <View style={{ flexDirection: I18nManager.isRTL ? 'row-reverse' : 'row', width: '100%', marginBottom: 20 }}>
-  <Text style={{ fontSize: 20 }}>כפתור</Text>
+  <Text style={{ fontSize: 20 }}>{I18nManager.isRTL ? 'true' : 'false'}</Text>
   <Button title="בדיקה" onPress={() => {}} />
 </View>
         </View>
